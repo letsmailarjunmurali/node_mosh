f1c5f08fc35e06e78b9bd0f49e73cbebd73c7533
arjunm <letsmailarjunmurali@gmail.com>
2021-03-19 11:21:20 +0530

    delete request

--- index.js
+++ index.js
@@ -69,6 +69,15 @@ app.put("/api/courses/:id", (req, res) => {
   res.send(course);
 });
 
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("The course with given ID was not found");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
+});
+
 function validateCourse(course) {
   const schema = { name: Joi.string().min(3).required() };
   return Joi.validate(course, schema);
