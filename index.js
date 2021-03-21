3339f6d30cb64f298123d82cb156e352cbe1661c
arjunm <letsmailarjunmurali@gmail.com>
2021-03-21 18:51:33 +0530

    get_repo

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ console.log("Before");
 
 getUser(1, (user) => {
   console.log(user);
+  getRepositories(user.githubUsername, (repo) => {
+    console.log(repo);
+  });
+
+  // Nested Callback
 });
 
 console.log("After");
@@ -10,5 +15,12 @@ function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading from database");
     callback({ id: id, githubUsername: "arjun" });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log("Calling Github Api");
+    callback(["repo1", "repo2", "repo3"]);
   }, 2000);
 }
