a0334e0494bb45dc2f0caa5f3f6c62ed10577161
arjunm <letsmailarjunmurali@gmail.com>
2021-03-21 12:11:37 +0530

    template engine using pug

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "./views");
+
 if (app.get("env") === "development") {
   app.use(morgan("tiny"));
   startupDebugger("Morgan enabled");
 }
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "First app", message: "Welcome" });
+});
 
 dbDebugger("Connected to the database");
 const port = process.env.PORT || 3000;
