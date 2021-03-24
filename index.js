c74cb90c062cbe41021b65e797daf394b26807f2
arjunm <letsmailarjunmurali@gmail.com>
2021-03-24 14:36:11 +0530

    retrieving_using_condition

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ const courseSchema = new mongoose.Schema({
 const Course = mongoose.model("Course", courseSchema);
 
 async function getCourses() {
-  return await Course.find({ isPublished: true, tags: "backend" })
-    .sort({ name: 1 })
-    .select({ name: 1, author: 1 });
+  return await Course.find({
+    isPublished: true,
+    tags: { $in: ["frontend", "backend"] },
+  })
+    .sort("-price")
+    .select("name author price");
 }
 
 async function run() {
