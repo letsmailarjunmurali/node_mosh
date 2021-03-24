166165e8c25ec55fc85c89aa7c79b3fa082cbcf5
arjunm <letsmailarjunmurali@gmail.com>
2021-03-24 08:47:52 +0530

    creating_the_document

---
+++ index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/test")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: "Angular",
+    author: "Mosh",
+    tags: ["node", "backend"],
+    isPublished: true,
+  });
+  const result = await course.save();
+  console.log(result);
+}
+createCourse();
