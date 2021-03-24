da252d0510fef4fbde207ac1f2cb3536121cbf6c
arjunm <letsmailarjunmurali@gmail.com>
2021-03-24 14:32:15 +0530

    retriving_courses_using_filter

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const courseSchema = new mongoose.Schema({
 const Course = mongoose.model("Course", courseSchema);
 
 async function getCourses() {
-  return await Course.find().sort({ name: 1 }).select({ name: 1, author: 1 });
+  return await Course.find({ isPublished: true, tags: "backend" })
+    .sort({ name: 1 })
+    .select({ name: 1, author: 1 });
 }
 
 async function run() {
